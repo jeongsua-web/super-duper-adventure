a6ebab38a32609b4b68580368e674feb1e852db6
손민경 <2024_136008@gmail.com>
2025-12-15 03:17:26 +0900

    .

--- lib/pages/home/views/main_home_view.dart
+++ lib/pages/home/views/main_home_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'dart:ui';
 import 'dart:convert';
 import '../controllers/main_home_controller.dart';
